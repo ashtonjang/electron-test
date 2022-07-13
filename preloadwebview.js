82e55099b1f5c96ef097b3f4f9e77c0e63e3cee1
JangSeungHyun <ashton2.3jang@gmail.com>
2022-07-13 20:13:23 +0900

    .

---
+++ preloadwebview.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function () {
+  document.querySelector("body").addEventListener("click", function (e) {
+    const atag = e.path.find((item) => item.nodeName === "A");
+
+    if (atag) {
+      if (atag.href && atag.target === "_blank") {
+        window.open(atag.href);
+      }
+      e.preventDefault();
+      e.stopPropagation();
+    }
+  });
+});
